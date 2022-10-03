983e6ff1b524d7749085ffa2b2c05cf8e920e9af
joseftango <4279@holbertonschool.com>
2022-10-03 16:06:47 +0100

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,10 +8,12 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 	{
 
-		if (!(node->left) || !(node->right))
-			return (1);
-
-		else
+		if (!node)
 			return (0);
+
+		if ((node->left) || (node->right))
+			return (0);
+		else
+			return (1);
 
 	}
