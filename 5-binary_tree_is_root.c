549b638b0b2eecbb5200dc019ed2379e38d530f5
joseftango <4279@holbertonschool.com>
2022-10-03 16:20:29 +0100

    5-binary_tree_is_root.c

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+* binary_tree_is_root - function that checks if the node is root
+* @node: given node
+* Return: 1 or 0
+**/
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+
+	if (!node)
+		return (0);
+
+	if (node->parent == NULL)
+		return (1);
+
+	else
+		return (0);
+
+}
