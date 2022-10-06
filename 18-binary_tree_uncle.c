f94306a6658557eb979cf50730802090bc56bca6
joseftango <4279@holbertonschool.com>
2022-10-06 21:56:37 +0100

    18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,7 +8,7 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 
-	if ((!node || !node->parent) || !node->parent->parent)
+	if ((!node || !node->parent))
 		return (NULL);
 
 	if (node == node->parent->parent->left->left ||
